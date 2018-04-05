9b8ba96a701e5b2b467e97c329c096aa9cbc5e3c
m4kvn <fun.masahiro@gmail.com>
2018-04-05 15:22:35 +0900

    Update version 0.0.4

--- esa.rb
+++ esa.rb
@@ -1,11 +1,11 @@
 REPOSITORY_URL="https://github.com/m4kvn/homebrew-esa".freeze
-HOMEBREW_ESA_VERSION="0.0.3".freeze
+HOMEBREW_ESA_VERSION="0.0.4".freeze
 
 class Esa < Formula
   desc "Search esa's articles"
   homepage "https://github.com/m4kvn/homebrew-esa"
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_ESA_VERSION}.tar.gz"
-  sha256 "2d1b36fe2ea7af5f8d76f977325845e0d5a27fa98875a91ea9d7593fffa0821b"
+  sha256 "64711602557300317a31099aac104ea76ce8f49928b22aa0928beb5921465d81"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_ESA_VERSION
 
   depends_on "jq"
