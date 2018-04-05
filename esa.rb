f5ec0977c588d9279c9760383059920716a4f114
m4kvn <fun.masahiro@gmail.com>
2018-04-05 12:08:55 +0900

    Create esa formula

---
+++ esa.rb
@@ -0,0 +1,19 @@
+REPOSITORY_URL="https:.//github.com/m4kvn/homebrew-esa".freeze
+HOMEBREW_ESA_VERSION="0.0.1".freeze
+
+class Esa < Formula
+  desc ""
+  homepage REPOSITORY_URL
+  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_ESA_VERSION}.tar.gz"
+  sha256 "c84a8d17591b23a1cfd5badec00e9b67210096607dc9102318252044eaa6fb3d"
+  head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_ESA_VERSION
+  depends_on ["jq", "fzf"]
+
+  def install
+      bin.install "bin/esa"
+  end
+
+  test do
+    system "false"
+  end
+end
