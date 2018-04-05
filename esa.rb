de5ca7d99aa7c6fba4b1747cbe9138c421ee9b11
m4kvn <fun.masahiro@gmail.com>
2018-04-05 14:16:47 +0900

    Fix formula problem

--- esa.rb
+++ esa.rb
@@ -1,9 +1,9 @@
 REPOSITORY_URL="https://github.com/m4kvn/homebrew-esa".freeze
-HOMEBREW_ESA_VERSION="0.0.2".freeze
+HOMEBREW_ESA_VERSION="0.0.3".freeze
 
 class Esa < Formula
   desc "Search esa's articles"
-  homepage REPOSITORY_URL
+  homepage "https://github.com/m4kvn/homebrew-esa"
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_ESA_VERSION}.tar.gz"
   sha256 "c84a8d17591b23a1cfd5badec00e9b67210096607dc9102318252044eaa6fb3d"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_ESA_VERSION
